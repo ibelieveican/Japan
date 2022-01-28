4da73b64499281e87180436ac0f5e434ace38eaf
xuweicheng <xuweicheng@baiwang.com>
2022-01-28 20:32:20 +0800

    创建路由

---
+++ src/components/pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+       denglu 
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'login',
+    data () {
+        return {
+           
+        }
+    },
+    methods: {
+    },
+    created () {
+    },
+   
+}
+</script>
+
+<style scoped>
+   
+</style>
